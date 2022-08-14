75e652e589ac8ef53828474c38dd07aa98f68e28
nmakes <nav.naveenvenkat@gmail.com>
2022-08-14 03:43:43 -0400

    Adding MNIST example

--- example.py
+++ example.py
@@ -3,7 +3,7 @@ import torch
 
 from pytorch_boiler import Boiler, overload
 from example_model import TinyResNet
-from example_dataloader import cifar10_dataloader
+from example_dataloader import mnist_dataloader
 
 
 class Trainer(Boiler):
@@ -29,8 +29,8 @@ class Trainer(Boiler):
 if __name__ == '__main__':
     model = TinyResNet(in_channels=1, hidden_channels=4, output_channels=10, num_layers=3, expansion_factor=2).cuda()
     optimizer = torch.optim.Adam(params=model.parameters(), lr=5e-4, weight_decay=1e-5)
-    train_dataloader = cifar10_dataloader(root='./data/cifar10', batch_size=4, train=True, shuffle=True, drop_last=True)
-    val_dataloader = cifar10_dataloader(root='./data/cifar10', batch_size=4, train=False, shuffle=False, drop_last=False)
+    train_dataloader = mnist_dataloader(root='./data/mnist', batch_size=256, train=True, shuffle=True, drop_last=True)
+    val_dataloader = mnist_dataloader(root='./data/mnist', batch_size=256, train=False, shuffle=False, drop_last=False)
 
     trainer = Trainer(model=model, optimizer=optimizer, train_dataloader=train_dataloader, val_dataloader=val_dataloader, 
                       epochs=10, save_path='./state_dict.pt', load_path=None).fit()
--- example_dataloader.py
+++ example_dataloader.py
@@ -24,6 +24,22 @@ def cifar10_dataloader(root='./data', train=True, batch_size=4, drop_last=True, 
 
     return loader
 
+
+def mnist_dataloader(root='./data', train=True, batch_size=4, drop_last=True, shuffle=True, num_workers=4):
+    if train:
+        transforms = tvt.Compose([
+            tvt.ToTensor(),
+            tvt.Normalize(0.5, 0.5)
+        ])
+    else:
+        transforms = tvt.Compose([
+            tvt.ToTensor(),
+            tvt.Normalize(0.5, 0.5)
+        ])
+
+    loader = DataLoader(
+        dataset=tvd.MNIST(root=root, train=train, transform=transforms, download=True),
+        batch_size=batch_size, shuffle=shuffle, num_workers=num_workers, drop_last=drop_last
     )
 
     return loader
